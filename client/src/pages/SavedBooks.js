b1c9e17f0504eeccf18bb573e5a52bfc945258be
Carlos Aragon <carlos.s.aragon12@gmail.com>
2019-11-30 20:54:18 -0800

    got most of the pages working but still unable to get google search to work unless I remove the render of the search status. Manually setup the googleBooks db and books collection and added various indexes manually to see if it would fix the issue but still no luck..

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -1,11 +1,10 @@
 import React, { Component } from "react";
-import DeleteBtn from "../components/Button";
-import Jumbotron from "../components/Jumbotron";
+import Button from "../components/Button";
+// import Jumbotron from "../components/Jumbotron";
 import API from "../utils/API";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 import { Col, Row, Container } from "../components/Grid";
 import { List, ListItem } from "../components/List";
-import { Input, TextArea, FormBtn } from "../components/Input";
 
 class Books extends Component {
   state = {
